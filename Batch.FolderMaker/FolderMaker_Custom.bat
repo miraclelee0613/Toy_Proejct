ec94cc9ba1f138a910d716ad8b7d9dca07165323
miraclelee0613 <107083419+miraclelee0613@users.noreply.github.com>
2022-11-01 20:32:57 +0900

    Update FolderMaker_Custom.bat
    edit "update versin"

--- Batch.FolderMaker/FolderMaker_Custom.bat
+++ Batch.FolderMaker/FolderMaker_Custom.bat
@@ -1,51 +1,51 @@
 @echo off
-rem ¸í·É¾î º¹Ã¢ ²û
+rem ëª…ë ¹ì–´ ë³µì°½ ë”
 
-rem ### ¹öÀüÁ¤º¸	:	v1.4.2_release for all
-rem ### ¸¶Áö¸· ¾÷µ¥ÀÌÆ®: 22.11.01.
-rem ### ÄÚµåÁ¤º¸	:	https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=superyeoju&logNo=221747717398
+rem ### ë²„ì „ì •ë³´	:	v1.0.0_release for all
+rem ### ë§ˆì§€ë§‰ ì—…ë°ì´íŠ¸: 22.11.01.
+
 rem ### updated by LJS 
 
 rem chcp 65001>nul
-rem Âü°í https://otrodevym.tistory.com/entry/windows-10-cmd-%EC%9D%B8%EC%BD%94%EB%94%A9-utf-8-%EC%84%A4%EC%A0%95-%EB%B0%A9%EB%B2%95-949-65001-1
+rem ì°¸ê³  https://otrodevym.tistory.com/entry/windows-10-cmd-%EC%9D%B8%EC%BD%94%EB%94%A9-utf-8-%EC%84%A4%EC%A0%95-%EB%B0%A9%EB%B2%95-949-65001-1
 
 setLocal EnableDelayedExpansion
-rem Âü°í https://kkamagistory.tistory.com/881
+rem ì°¸ê³  https://kkamagistory.tistory.com/881
 
-rem 0 = °ËÁ¤»ö 8 = È¸»ö
-rem 1 = ÆÄ¶û»ö 9 = ¿¬ÇÑ ÆÄ¶û»ö
-rem 2 = ÃÊ·Ï»ö A = ¿¬ÇÑ ÃÊ·Ï»ö
-rem 3 = ¿Á»ö B = ¿¬ÇÑ ¿Á»ö
-rem 4 = »¡°­»ö C = ¿¬ÇÑ »¡°­»ö
-rem 5 = ÀÚÁÖ»ö D = ¿¬ÇÑ ÀÚÁÖ»ö
-rem 6 = ³ë¶û»ö E = ¿¬ÇÑ ³ë¶û»ö
-rem 7 = Èò»ö F = ¹àÀº Èò»ö
+rem 0 = ê²€ì •ìƒ‰ 8 = íšŒìƒ‰
+rem 1 = íŒŒëž‘ìƒ‰ 9 = ì—°í•œ íŒŒëž‘ìƒ‰
+rem 2 = ì´ˆë¡ìƒ‰ A = ì—°í•œ ì´ˆë¡ìƒ‰
+rem 3 = ì˜¥ìƒ‰ B = ì—°í•œ ì˜¥ìƒ‰
+rem 4 = ë¹¨ê°•ìƒ‰ C = ì—°í•œ ë¹¨ê°•ìƒ‰
+rem 5 = ìžì£¼ìƒ‰ D = ì—°í•œ ìžì£¼ìƒ‰
+rem 6 = ë…¸ëž‘ìƒ‰ E = ì—°í•œ ë…¸ëž‘ìƒ‰
+rem 7 = í°ìƒ‰ F = ë°ì€ í°ìƒ‰
 rem color 09
-rem Ã¹Â° ÀÚ¸®´Â ¹è°æ, µÑÂ° ÀÚ¸®´Â ±ÛÀÚ»ö
+rem ì²«ì§¸ ìžë¦¬ëŠ” ë°°ê²½, ë‘˜ì§¸ ìžë¦¬ëŠ” ê¸€ìžìƒ‰
 
 rem mode con cols=60 lines=30
-rem ÄÜ¼ÖÃ¢ Å©±â ÁöÁ¤
+rem ì½˜ì†”ì°½ í¬ê¸° ì§€ì •
 
 rem ####################################
 
 cd /d %~dp0
-rem ÇöÀç ÆÄÀÏ À§Ä¡ °æ·Î¸¦ ÀÛ¾÷ À§Ä¡·Î
+rem í˜„ìž¬ íŒŒì¼ ìœ„ì¹˜ ê²½ë¡œë¥¼ ìž‘ì—… ìœ„ì¹˜ë¡œ
 
-title ÇöÀç °æ·Î : %cd%
+title í˜„ìž¬ ê²½ë¡œ : %cd%
 :notice
-rem echo 	## ¾÷µ¥ÀÌÆ® ³»¿ª ##
+rem echo 	## ì—…ë°ì´íŠ¸ ë‚´ì—­ ##
 
-rem 		## 1.0.0	Ã¹ÆÄÀÏ »ý¼º
+rem 		## 1.0.0	ì²«íŒŒì¼ ìƒì„±
 
 :start
 set %f_name%=""
 echo.
-echo 		## Æú´õ ¿¬¼Ó »ý¼º±âÀÔ´Ï´Ù. ##
-echo 		## »ý¼ºÇÒ Æú´õ¸íÀ» ÀÔ·ÂÇÏ°í ¿£ÅÍ¸¦ Ä¡¸é Æú´õ »ý¼º ##
-echo 		## ÀÔ·Â¾øÀÌ ¿£ÅÍ¸¦ Ä¡¸é ÇÁ·Î±×·¥ Á¾·áÀÔ´Ï´Ù. ##
+echo 		## í´ë” ì—°ì† ìƒì„±ê¸°ìž…ë‹ˆë‹¤. ##
+echo 		## ìƒì„±í•  í´ë”ëª…ì„ ìž…ë ¥í•˜ê³  ì—”í„°ë¥¼ ì¹˜ë©´ í´ë” ìƒì„± ##
+echo 		## ìž…ë ¥ì—†ì´ ì—”í„°ë¥¼ ì¹˜ë©´ í”„ë¡œê·¸ëž¨ ì¢…ë£Œìž…ë‹ˆë‹¤. ##
 echo.
-echo 		## »ý¼ºÇÒ Æú´õ¸íÀ» ÀÔ·ÂÇØÁÖ¼¼¿ä ##
-set /p f_name=Æú´õ¸í :
+echo 		## ìƒì„±í•  í´ë”ëª…ì„ ìž…ë ¥í•´ì£¼ì„¸ìš” ##
+set /p f_name=í´ë”ëª… :
 if "%f_name%" == "" (
 	exit
 ) else (
